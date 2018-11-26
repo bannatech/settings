15a82b705fbba973c2210cfbc7a57e4de0dcecd1
Aftix <gameraftexploision@gmail.com>
2018-11-26 12:31:53 -0600

    changed to ftp

--- backup.sh
+++ backup.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-ftpfs="/ftp"
-mkdir -p "$ftpfs"
-curlftpfs 192.168.0.1 "$ftpfs"
-backup="$ftpfs/sda/Wyatt/$(date +%Y_%m_%d_%T)/"
+backup="sda/Wyatt/$(date +%Y_%m_%d_%T)/"
 tar cvJf /boot.txz /boot/
 tar cvJf /etc.txz --exclude /etc/dolphin-iso /etc/
 tar cvJf /var.txz --exclude /var/cache --exclude /var/lib /var/
 tar cvJf /share.txz /shared/
 tar cvJf /home.txz --exclude /home/aftix/torrent --exclude /home/aftix/.local/share/Steam /home/
 tar cvJf /root.txz /root/
-mv /boot.txz "$backup"
-mv /etc.txz "$backup"
-mv /var.txz "$backup"
-mv /share.txz "$backup"
-mv /home.txz "$backup"
-mv /root.txz "$backup"
-fusermount -u "$ftpfs"
-rmdir "$ftpfs"
+ftp -n <<EOF
+open 192.168.0.1
+user anonymous aaaa
+quote mkdir $backup
+quote cd $backup
+put /boot.txz
+put /etc.txz
+put /var.txz
+put /share.txz
+put /home.txz
+put /root.txz
+EOF
+rm /boot.txz
+rm /etc.txz
+rm /var.txz
+rm /share.txz
+rm /home.txz
+rm /root.txz
