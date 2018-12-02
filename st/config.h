409cd020ddb4e714f15414caaf4c85cdafe5ac79
Aftix <gameraftexploision@gmail.com>
2018-12-01 19:16:24 -0600

    changed to inconsolata font

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DroidSansMono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Inconsolata:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
