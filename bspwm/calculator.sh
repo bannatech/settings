61829cd2454dbde273510c278ac207824ed60e90
Aftix <gameraftexploision@gmail.com>
2019-05-12 17:05:58 -0500

    Changed from julia to dc (RPN)

--- bspwm/calculator.sh
+++ bspwm/calculator.sh
@@ -2,8 +2,8 @@
 
 . $HOME/.config/dmenurc
 
-eqn=$(xsel -o -b | dmenu $DOPTS -p "Calculate (julia): ")
-result=$(julia -E "$eqn")
+eqn=$(xsel -o -b | dmenu $DOPTS -p "Calculate (dc): ")
+result=$(printf "5 k %s p" "$eqn" | dc)
 
 printf "%s" "$result" | dmenu $DOPTS -p "Result: " >/dev/null
 printf "%s" "$result" | xsel -i -b
