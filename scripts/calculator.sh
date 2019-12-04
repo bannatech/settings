313af48a8c65dc652d4afc337eca1c8b3e072050
aftix <gameraftexploision@gmail.com>
2019-12-04 17:11:55 -0600

    Updated script

--- scripts/calculator.sh
+++ scripts/calculator.sh
@@ -2,8 +2,8 @@
 
 . $HOME/.config/dmenurc
 
-eqn=$(xsel -o -b | dmenu $DOPTS -p "Calculate (bc): ")
-result=$(printf "scale = 5 \n %s\n" "$eqn" | bc | sed -e 's/\.\{0,1\}0\+$//')
+eqn=$(xsel -o -b | dmenu $DOPTS -p "Calculate: ")
+result=$(printf "%s" "$eqn" | $HOME/.scripts/calc)
 
 printf "%s" "$result" | dmenu $DOPTS -p "Result: " >/dev/null
 printf "%s" "$result" | xsel -i -b
