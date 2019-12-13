881c18b78976e969ae1d989ce5441c75d8ab6318
aftix <gameraftexploision@gmail.com>
2019-12-13 13:45:16 -0600

    Made getvol more effecient

--- scripts/getvol.sh
+++ scripts/getvol.sh
@@ -10,24 +10,15 @@ printf "%s%s" "$VOL" "$AFTER"
 
 IFS=" "
 
+SED1='s/repeat/r/ ; s/single/s/ ; s/random/z/; s/consume/c/'
+SED2='s/volume: [[:digit:]]\{1,2\}%// ; s/[[:alpha:]]: off//g ; s/: on//g'
+
 printf " | "
 if [ "$(mpc | wc -l)" -gt 1 ] ; then
   mpc | awk '/^\[/ {print $1}' | tr -d '\n' | sed 's/playing/▶/ ; s/paused/P/'
   printf " "
   mpc | head -n 1 | tr -d '\n'
-  ATTR=""
-  if mpc | grep -q "repeat: on" ; then
-    ATTR="$ATTR"r
-  fi
-  if mpc | grep -q "random: on" ; then
-    ATTR="$ATTR"z
-  fi
-  if mpc | grep -q "single: on" ; then
-    ATTR="$ATTR"s
-  fi
-  if mpc | grep -q "consume: on" ; then
-    ATTR="$ATTR"c
-  fi
 
-  test "$ATTR" && printf " [%s]" "$ATTR"
+  mpc | tail -n 1 | grep -q ": on" &&
+  mpc | sed -n "$SED1;/volume/p" | sed "$SED2" | tr -d ' ' | xargs printf " [%s]"
 fi
