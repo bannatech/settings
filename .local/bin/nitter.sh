0ff1444c2d04abb91d321e89ed14ca70dd1dfbbc
aftix <aftix@aftix.xyz>
2020-08-16 11:24:48 -0500

    added bookmarks

--- .local/bin/nitter.sh
+++ .local/bin/nitter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ "$1" = "nitter" ] && (xsel -ob | sed --posix -e 's@\(https\{0,1\}://\(www\.\)\{0,1\}\)nitter.net@\1twitter.com@g ; s@\(https\{0,1\}://\(www\.\)\{0,1\}\)invidio.us@\1youtube.com@g' | xsel -ib)
-[ "$1" = "nitter" ] || (xsel -ob | sed --posix -e 's@\(https\{0,1\}://\(www\.\)\{0,1\}\)twitter.com@\1nitter.net@g ; s@\(https\{0,1\}://\(www\.\)\{0,1\}\)youtube.com@\1invidio.us@g' | xsel -ib)
+[ "$1" = "nitter" ] && (xsel -ob | sed --posix -e 's@\(https\{0,1\}://\(www\.\)\{0,1\}\)nitter.net@\1twitter.com@g ; s@\(https\{0,1\}://\(www\.\)\{0,1\}\)invidiou.site@\1youtube.com@g' | xsel -ib)
+[ "$1" = "nitter" ] || (xsel -ob | sed --posix -e 's@\(https\{0,1\}://\(www\.\)\{0,1\}\)twitter.com@\1nitter.net@g ; s@\(https\{0,1\}://\(www\.\)\{0,1\}\)youtube.com@\1invidiou.site@g' | xsel -ib)
