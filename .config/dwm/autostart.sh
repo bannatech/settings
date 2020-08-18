f1369bde3d4ba66d6c3d2bbc0ae8500604a14c9e
aftix <aftix@aftix.xyz>
2020-08-18 11:18:08 -0500

    fixed path for xmodmap

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -3,7 +3,7 @@ cd $HOME
 picom &
 dunst &
 xautolock -time 10 -locker "$HOME/.local/bin/locker.sh" &
-xmodmap $XDG_CONFIG_HOME/.Xmodmap
+xmodmap $HOME/.config/.Xmodmap
 xset r rate 290 50
 dwmblocks &
 sxhkd -m 2 &
